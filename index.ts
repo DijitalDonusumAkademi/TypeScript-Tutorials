e11ebcb879a41ddd5ab708a837fae7dfd9ebafaf
Zeki SERT <zeki.sert@prestijyazilim.com.tr>
2020-12-14 22:25:44 +0300

    Executing TypeScript Code

--- index.ts
+++ index.ts
@@ -1,1 +1,11 @@
-import axios from 
+import axios from 'axios';
+
+const url = 'http://jsonplaceholder.typicode.com/todos/1';
+
+axios.get(url).then(response => {
+    console.log(response.data);
+});
+
+// tsc index.ts
+// node index.js
+// ts-node index.ts
